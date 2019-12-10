9cf087f6dcc6a406c581bf4e89f5d924cab4acab
Kazuhiro Ebara <romsound040220@gmail.com>
2019-12-10 12:41:40 +0900

    first commit

---
+++ src/renderer/pages/hoge.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="e-nuxt-container">
+    <div class="e-nuxt-container-main">
+      <div class="hoge">hoge</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { remote } from 'electron'
+import SystemInformation from '@/components/SystemInformation.vue'
+
+export default {
+  components: {
+    SystemInformation
+  },
+  data () {
+    return {
+      externalContent: ''
+    }
+  },
+  methods: {
+    openURL (url) {
+      remote.shell.openExternal(url)
+    }
+  }
+}
+</script>
+
+<style>
+.e-nuxt-container {
+  min-height: calc(100vh - 50px);
+  background: linear-gradient(to right, #ece9e6, #ffffff);
+  font-family: Helvetica, sans-serif;
+}
+
+.e-nuxt-container-main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: calc(100vh - 50px);
+}
+
+.hoge {
+  font-size: 100px;
+  font-weight: bold;
+}
+</style>
